71bb42d260780fb901764954cd088d020c9d7d9b
Cody Mitchell <codyshanemitchell@gmail.com>
2018-10-03 20:14:06 -0700

    Added two new PDF templates and functionality to populate them both.

---
+++ background.js
@@ -0,0 +1,8 @@
+"use strict";
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create("index.html", {
+    id: "main",
+    bounds: { width: 620, height: 500 }
+  });
+});
