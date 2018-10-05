f3a3d1515da0c53e39bf1429a6ceacf90657f708
Cody Mitchell <codyshanemitchell@gmail.com>
2018-10-05 15:23:06 -0700

    Cleaned up code. Moved a few unused scripts to the gitignore

--- background.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create("index.html", {
-    id: "main",
-    bounds: { width: 620, height: 500 }
-  });
-});
